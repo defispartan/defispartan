d272c4efce1810d7e84bad26cb5b183c7421acf8
Andrew Schmidt <andrew@aave.com>
2022-11-27 17:08:34 -0600

    feat: use experimental app directory

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+
+            <main className={styles.main}>
+                <p className={styles.description}>
+                    <code className={styles.code}>welcome</code>
+                </p>
+            </main>
+        </div>
+    )
+}
