f15296eced3a90dc0bbb5b58cd140e886ec7932d
Andrew Schmidt <andrew@aave.com>
2022-11-28 00:18:01 -0600

    feat: add rpc and contract setup

---
+++ lib/config.ts
@@ -0,0 +1,25 @@
+import { Contract } from "ethers";
+import { RotationProvider } from "./helpers/rotationProvider";
+import LensHubABI from "./abis/lensHubImplementation.json";
+
+export const PROFILE_ID = 10859;
+
+const polygonProviders = [
+  "https://polygon-rpc.com",
+  "https://polygon-mainnet.public.blastapi.io",
+  "https://rpc-mainnet.matic.quiknode.pro",
+];
+
+export const IPFS_GATEWAY = "https://lens.infura-ipfs.io/ipfs/";
+
+export const polygonProvider = new RotationProvider(polygonProviders, 137);
+
+// Deployment block of LensHubProxy
+export const STARTING_BLOCK = 28384641;
+const lensHubProxyAddress = "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d";
+
+export const lensHubProxyContract = new Contract(
+  lensHubProxyAddress,
+  LensHubABI,
+  polygonProvider
+);
